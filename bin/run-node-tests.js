7fc6d2150a978dfc1bd545e4e06d6f28efa6a5fd
Kris Selden <kris.selden@gmail.com>
2017-01-23 13:18:02 -0800

    Make tap output more testem friendly

--- bin/run-node-tests.js
+++ bin/run-node-tests.js
@@ -3,14 +3,25 @@
 var QUnit = global.QUnit = require('qunitjs');
 var path = require('path');
 var glob = require('glob');
-var qunitTap = require('qunit-tap');
-var tap = qunitTap(QUnit, function() { console.log.apply(console, arguments); });
 
-tap.done = function(results) {
-  if (results.failed) {
+var count = 0;
+QUnit.log(function( details ) {
+  count++;
+  var message = details.result ? "ok " : "not ok ";
+  message += count + " "
+  message += details.module + " - " + details.name;
+  if (details.message) {
+    message += ": " + details.message;
+  }
+  console.log(message);
+});
+
+QUnit.done(function( details ) {
+  console.log("1.." + count);
+  if (details.failed) {
     process.exit(1);
   }
-}
+});
 
 glob.sync('./dist/node_modules/@glimmer/node/tests/**/*-test.js').forEach(function(file) {
   require(path.resolve(file));
